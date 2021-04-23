786e8901a31baab986de5b12fd9935de9687820a
kenny <kenny@DESKTOP-LVLCKMK.home>
2021-04-23 09:16:41 -0700

    start on the minimax algorithm

---
+++ miniMax.java
@@ -0,0 +1,19 @@
+public class miniMax {
+	int maximumDepth, aiPlayerID;
+	
+	public miniMax(int playerID) {
+		aiPlayerID = playerID;
+	}
+	
+	// minimum method 
+	public min(int depth) {
+		Random randomplay = new Random();
+		// check if the game has ended or reached maximum depth
+		// if not then calculate all the child moves
+	}
+	
+	public max(int depth) {
+		Random randomplay = new Random();
+		// similar to the minimum method but different moves
+	}
+}
