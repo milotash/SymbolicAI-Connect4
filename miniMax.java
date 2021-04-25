50c57068584545c0ee6f67731531a3b232894679
Kenny Chhoeun <59298127+lowtek232@users.noreply.github.com>
2021-04-24 20:39:43 -0700

    started working on minimum portion of minmax

--- miniMax.java
+++ miniMax.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class miniMax {
 	int maximumDepth, aiPlayerID;
 	
@@ -6,11 +8,26 @@ public class miniMax {
 	}
 	
 	// minimum method 
-	public min(int depth) {
-		Random randomplay = new Random();
+	public Gameplay minAlgorithm(int depth, GameState gamestate) {
+		Random random = new Random();
 		// check if the game has ended or reached maximum depth
+		if((gameBoard.ifGameOver()) || (depth == maximumDepth)) {
+			// new gameplay object represent base move
+			Gameplay primaryMove = new Gameplay();
+			return primaryMove.expansionMove();
+		} else {
+			ArrayList<GameState> childen = new ArrayList<GameState>();
+			// create another gameplay object that represenet the mininum move
+			Gameplay mininumMove = new Gameplay();
+			/* for everything under the size of the children get
+			 * and get the value for what is higher value the child or lower depth
+			 * the one with lowest is chosen and returned
+			 * 
+			 */
+			
+		}
 		// if not then calculate all the child moves
-	}
+	} // end of min function
 	
 	public max(int depth) {
 		Random randomplay = new Random();
