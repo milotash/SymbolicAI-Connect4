1fd916ced8abf1912066933a5d60520d62f760f8
milotash <83149550+milotash@users.noreply.github.com>
2021-04-24 22:05:17 -0700

    Completing GamePlay.java
    Completing methods and adding setters for rows, columns, and key. As well as a get method to get key value.

--- GamePlay.java
+++ GamePlay.java
@@ -4,21 +4,58 @@ public class GamePlay {
 	int columns;
 	private int key;
 	
+	//constructor
 	public GamePlay(){
 		rows = -1;
 		columns = -1;
 		key = 0;
 	}
 	
+	//method for move expansion
 	public GamePlay expansionMove(int r, int c, int k){
-		return null;
+		GamePlay expansionMove = new GamePlay();
+		expansionMove.rows = r;
+		expansionMove.columns = c;
+		expansionMove.key = k;
+		return expansionMove;
 	}
 	
+	//method for move comparing with MiniMax
 	public GamePlay compareMove(int k){
-		return null;
+		GamePlay compareMove = new GamePlay();
+		compareMove.rows = -1;
+		compareMove.columns = -1;
+		compareMove.key = k;
+		return compareMove;
 	}
 	
+	//method for finished move
 	public GamePlay finishMove(int r, int c){
-		return null;
+		GamePlay finishMove = new GamePlay();
+		finishMove.rows = r;
+		finishMove.columns = c;
+		finishMove.key = -1;
+		return finishMove;
 	}
+	
+	//set rows
+	public void mutateRows(int r) {
+		rows = r;
+	}
+	
+	//set columns
+	public void mutateColumns(int c) {
+		columns = c;
+	}
+	
+	//set key
+	public void mutateKey(int k) {
+		key = k;
+	}
+	
+	//getkey
+	public int getKey() {
+		return key;
+	}
+	
 }
